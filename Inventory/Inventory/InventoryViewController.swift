e836285c0b980f2f5f9fd869bdccfbaa2ed6b7e5
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-11-06 21:44:38 -0600

    finished

--- Inventory/Inventory/InventoryViewController.swift
+++ Inventory/Inventory/InventoryViewController.swift
@@ -10,26 +10,29 @@ import UIKit
 
 class InventoryViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
-    @IBOutlet weak var inventoryTableView: UITableViewCell!
-    
+    @IBOutlet weak var inventoryTableView: UITableView!
     var inventory = Inventory.load(from: "inventory")
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   func numberOfSections(in tableView: UITableView) -> Int {
+       return 1
+   }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return inventory?.products.count ?? 0
     }
-    */
-
+       
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "inventoryCell", for: indexPath)
+           
+           if let product = inventory?.products[indexPath.row] {
+                cell.textLabel?.text = product.title
+                cell.detailTextLabel?.text = "In stock: \(product.stockedQuantity), Price: $\(String(format: "%.2f", product.price)), Category: \(product.category)"
+           }
+           return cell
+    }
 }
--- Inventory/Inventory/Model/Inventory.swift
+++ Inventory/Inventory/Model/Inventory.swift
@@ -12,14 +12,14 @@ struct Inventory: Decodable {
     var status : String
     var products : [Product]
     
-    static func load(from filename: String) -> Inventory? {
-        guard let url = Bundle.main.url(forResource: filename, withExtension: "json"),
-            let data = try? Data(contentsOf: url) else {
-                return nil
+        static func load(from filename: String) -> Inventory? {
+            guard let url = Bundle.main.url(forResource: filename, withExtension: "json"),
+                let data = try? Data(contentsOf: url) else {
+                    return nil
+            }
+            let jsonDecoder = JSONDecoder()
+            return try? jsonDecoder.decode(Inventory.self, from: data)
         }
-        let jsonDecoder = JSONDecoder()
-        return try? jsonDecoder.decode(Inventory.self, from: data)
-    }
 }
 
 struct Product: Decodable {
